d57e33e0897a3c54eb6fa7fed00eef26436157e5
likuor <likuor0528@gmail.com>
2023-12-25 14:38:30 -0800

    add: navbar

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,34 @@
+import Link from 'next/link';
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header className="bg-gray-700 p-4 text-white">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="font-semibold text-2xl">
+          <Link href="/">Next-sns</Link>
+        </h1>
+        <nav>
+          <ul className="flex space-x-4">
+            <>
+              <Link
+                href="/login"
+                className="bg-white text-gray-900 py-2 px-3 rounded-lg font-medium"
+              >
+                Login
+              </Link>
+              <Link
+                href="/signup"
+                className="bg-white text-gray-900 py-2 px-3 rounded-lg font-medium"
+              >
+                Signup
+              </Link>
+            </>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
