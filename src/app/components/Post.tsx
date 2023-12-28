78047e9616c23e11d4ecd54032015dd0926d855a
likuor <likuor0528@gmail.com>
2023-12-28 12:47:07 -0800

    add: link to users

--- src/app/components/Post.tsx
+++ src/app/components/Post.tsx
@@ -2,6 +2,7 @@
 
 import { PostProps } from '@/types';
 import Image from "next/image";
+import Link from 'next/link';
 
 const Post = (props: PostProps) => {
   const { post } = props
@@ -10,13 +11,17 @@ const Post = (props: PostProps) => {
     <div className="bg-white shadow-md rounded p-4 mb-4">
       <div className="mb-4">
         <div className="flex items-center mb-2">
-          <Image
-            className="w-10 h-10 rounded-full mr-2"
-            src={post.author.profile.profileImageUrl}
-            alt="User Avatar"
-            width={40}
-            height={40}
-          />
+          <Link
+            href={`profile/${post.authorId}`}
+          >
+            <Image
+              className="w-10 h-10 rounded-full mr-2"
+              src={post.author.profile.profileImageUrl}
+              alt="User Avatar"
+              width={40}
+              height={40}
+            />
+          </Link>
           <div>
             <h2 className="font-semibold text-md">{post.author?.username}</h2>
             <p className="text-gray-500 text-sm">{new Date(post.createdAt).toLocaleString()}</p>
@@ -24,7 +29,7 @@ const Post = (props: PostProps) => {
         </div>
         <p className="text-gray-700">{post.content}</p>
       </div>
-    </div>
+    </div >
   );
 };
 
